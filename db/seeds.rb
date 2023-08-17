85b0cdbcf41b2c4500900fbc4a4ee5a96c81b3f4
Malin Schadel <malin.schadel@gmail.com>
2023-08-17 14:11:40 +0200

    add tag and plant_tag models

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ Garden.create!(
 puts "#{Garden.count} gardens created."
 
 Plant.create!(
-  name: 'Pink Flower',
-  image_url: 'https://plus.unsplash.com/premium_photo-1678912442478-bbb088527ab0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2FyZGVufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+  name: 'Fern',
+  image_url: 'https://images.unsplash.com/photo-1599148401005-fe6d7497cb5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=654&q=80',
   garden: Garden.first
 )
 Plant.create!(
@@ -23,8 +23,8 @@ Plant.create!(
   garden: Garden.first
 )
 Plant.create!(
-  name: 'More Flowers',
-  image_url: 'https://plus.unsplash.com/premium_photo-1678836292789-6bb8d1f14a43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGdhcmRlbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+  name: 'Cacti',
+  image_url: 'https://images.unsplash.com/photo-1463936575829-25148e1db1b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2117&q=80',
   garden: Garden.first
 )
 puts "#{Plant.count} gardens created."
